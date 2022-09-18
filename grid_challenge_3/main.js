8f51624845155d0c2662d68aca615be92603c200
Vijay KC <vj.kc2007@gmail.com>
2022-09-18 13:06:13 +0300

    Added 'To Top' Button

--- grid_challenge_3/main.js
+++ grid_challenge_3/main.js
@@ -6,6 +6,7 @@ const menuIcon = document.querySelector(".menu");
 const nav = document.querySelector("nav");
 
 const menuList = document.querySelectorAll("nav ul li");
+const btnTop = document.querySelector("#toTop");
 
 imgs.forEach((img) => {
   img.addEventListener("click", function () {
@@ -30,4 +31,25 @@ menuIcon.addEventListener("click", menuToggle);
 for (const list of menuList) {
   list.addEventListener("click", menuToggle);
 }
-XMLDocumentx;
+
+window.onscroll = function () {
+  revealToTopBtn();
+};
+
+function revealToTopBtn() {
+  if (
+    document.documentElement.scrollTop > 200 ||
+    document.body.scrollTop > 200
+  ) {
+    btnTop.style.display = "block";
+  } else {
+    btnTop.style.display = "none";
+  }
+}
+
+const getToTop = () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
+
+btnTop.addEventListener("click", getToTop);
