525f29563198deda80863bb0c83cdcd369aeb1f4
Vijay KC <vj.kc2007@gmail.com>
2022-09-21 21:49:04 +0300

    Merged gallery page

--- portfolio_website/main.js
+++ portfolio_website/main.js
@@ -1,5 +1,4 @@
 const iconExit = document.getElementById("icon-exit");
-const nav = document.querySelector("nav");
 const toggle = document.getElementById("icon-toggle");
 
 const skills = document.querySelectorAll(".skill");
@@ -7,13 +6,13 @@ const expandBtn = document.querySelectorAll(".expand");
 const skillBody = document.querySelectorAll(".skill--body");
 const scrollTop = document.querySelector("#to-top");
 
-toggle.addEventListener("click", () => {
-  if (nav.classList.contains("hide")) {
-    nav.classList.remove("hide");
-  } else {
-    nav.classList.add("hide");
-  }
-});
+const imgs = document.querySelectorAll(".gallery-container img");
+const overlay = document.querySelector(".overlay");
+
+const mobMenu = document.querySelector(".mobile-menu");
+
+const menuList = document.querySelectorAll("nav ul li");
+const btnTop = document.querySelector("#toTop");
 
 skills.forEach((header) => {
   header.addEventListener("click", () => {
@@ -21,20 +20,52 @@ skills.forEach((header) => {
   });
 });
 
-const scrollToTop = () => {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    scrollTop.style.display = "block";
-  } else {
-    scrollTop.style.display = "none";
-  }
-};
+imgs.forEach((img) => {
+  img.addEventListener("click", function () {
+    overlay.style.backgroundImage = "url(" + img.src + ")";
+    overlay.style.backgroundSize = "30%";
+    overlay.style.display = "block";
+  });
+});
 
-const moveToTop = () => {
+function menuToggle() {
+  if (nav.classList.contains("mobile-menu")) {
+    nav.classList.remove("mobile-menu");
+    menuIcon.textContent = "menu";
+  } else {
+    nav.classList.add("mobile-menu");
+    menuIcon.textContent = "close";
+  }
+}
+
+toggle.addEventListener("click", menuToggle);
+
+for (const list of menuList) {
+  list.addEventListener("click", () => {
+    if (nav.classList.contains(".mobile-menu")) {
+      nav.classList.remove(".mobile-menu");
+    }
+  });
+}
+
+function revealToTopBtn() {
+  if (
+    document.documentElement.scrollTop > 200 ||
+    document.body.scrollTop > 200
+  ) {
+    btnTop.style.display = "block";
+  } else {
+    btnTop.style.display = "none";
+  }
+}
+
+const getToTop = () => {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 };
 
 window.onscroll = function () {
-  scrollToTop();
+  revealToTopBtn();
 };
-scrollTop.addEventListener("click", moveToTop);
+
+btnTop.addEventListener("click", getToTop);
