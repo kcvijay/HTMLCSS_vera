10893a762c55ede05f0827e5c271d133b207d71e
Vijay KC <vj.kc2007@gmail.com>
2022-09-22 11:11:25 +0300

    minor technical bug fixes

--- portfolio_website/main.js
+++ portfolio_website/main.js
@@ -1,8 +1,4 @@
-const iconExit = document.getElementById("icon-exit");
-const toggle = document.getElementById("icon-toggle");
-
 const skills = document.querySelectorAll(".skill");
-const expandBtn = document.querySelectorAll(".expand");
 const skillBody = document.querySelectorAll(".skill--body");
 const scrollTop = document.querySelector("#to-top");
 
@@ -16,20 +12,23 @@ const iconToggle = document.querySelector("#icon-toggle");
 const menuList = document.querySelectorAll("nav ul li a");
 const btnTop = document.querySelector("#toTop");
 
+//Skill items accordian activation
 skills.forEach((header) => {
   header.addEventListener("click", () => {
     header.classList.toggle("active");
   });
 });
 
+//Zooming every image item on click.
 imgs.forEach((img) => {
   img.addEventListener("click", function () {
     overlay.style.backgroundImage = "url(" + img.src + ")";
-    overlay.style.backgroundSize = "80%";
+    overlay.style.backgroundSize = "70%";
     overlay.style.display = "block";
   });
 });
 
+//Toggling a responsive mobile menu.
 function menuToggle() {
   if (nav.classList.contains("mobile-menu")) {
     nav.classList.remove("mobile-menu");
@@ -40,22 +39,7 @@ function menuToggle() {
   }
 }
 
-iconToggle.addEventListener("click", menuToggle);
-
-for (const list of menuList) {
-  list.addEventListener("click", () => {
-    nav.classList.remove("mobile-menu");
-  });
-}
-
-// menuList.forEach((list) => {
-//   list.addEventListener("click", () => {
-//     if (nav.classList.contains("mobile-menu")) {
-//       nav.classList.remove("mobile-menu");
-//     }
-//   });
-// });
-
+//Reavealing the 'to-top' button afterscrolling down 200px .
 function revealToTopBtn() {
   if (
     document.documentElement.scrollTop > 200 ||
@@ -67,6 +51,7 @@ function revealToTopBtn() {
   }
 }
 
+//Getting to top after clicking 'to-top' button.
 const getToTop = () => {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
@@ -76,4 +61,20 @@ window.onscroll = function () {
   revealToTopBtn();
 };
 
+iconToggle.addEventListener("click", menuToggle);
+
+for (const list of menuList) {
+  list.addEventListener("click", () => {
+    nav.classList.remove("mobile-menu");
+    iconToggle.textContent = "menu";
+  });
+}
+
 btnTop.addEventListener("click", getToTop);
+// menuList.forEach((list) => {
+//   list.addEventListener("click", () => {
+//     if (nav.classList.contains("mobile-menu")) {
+//       nav.classList.remove("mobile-menu");
+//     }
+//   });
+// });
